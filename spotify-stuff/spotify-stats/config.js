9c13f67aa21b21ca7bb5ad163cd5a30404105db9
root <root@instance-1.us-east1-b.c.project1-227020.internal>
2019-01-30 20:50:13 +0000

    add link to guitar stuff to main page

--- spotify-stuff/spotify-stats/config.js
+++ spotify-stuff/spotify-stats/config.js
@@ -1,4 +1,4 @@
 const cfgparams = {
-    redirectUri: "http:%2F%2Flocalhost:8000",
+    redirectUri: "http:%2F%2F35.231.103.214/spotify-stuff/spotify-stats",
     clientID: "acc93cedd94a4f6d820e7e45ccd75e7c"
 };
